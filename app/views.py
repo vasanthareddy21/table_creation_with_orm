8176002e90063d310c7104867a08fa03383bfbe2
vasanthareddy21 <vasanthareddya9@gamil.com>
2023-08-26 14:01:23 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,25 +2,59 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.models import *
+from django.db.models.functions import Length
 
 def Display_AccessRecords(request):
 
+    #order_by
     QSARO=AccessRecords.objects.all()
+    QSARO=AccessRecords.objects.all().order_by('name')
+
     d={'QSARO':QSARO}
     return render(request,'Display_AccessRecords.html',d)
 
 def Display_Webpage(request):
     
     QSWO=Webpage.objects.all()
+    #order_by
+    QSWO=Webpage.objects.all().order_by('Topic_name')#Ascending order
+    QSWO=Webpage.objects.all().order_by('-Topic_name')#Descending order
+    
+    #Based on length
+    QSWO=Webpage.objects.all().order_by(Length('name'))#Acending order
+    QSWO=Webpage.objects.all().order_by(Length('name').desc()) #Descending order
+
+    #To fetch the any number of rows you want using positive indexing in slicing(we can not use negative indexing)
+    QSWO=Webpage.objects.all()[:2]
+    QSWO=Webpage.objects.all()[2:5]
+
+    #We use exclude method opposite to the filter method[except cricket anything it will fetch]
+    QSWO=Webpage.objects.exclude(Topic_name='cricket')
+
+    #we use filter metod[it will fetch only Boxing]
+    QSWO=Webpage.objects.exclude(Topic_name='Boxing')
+
     d={'QSWO':QSWO}
     return render(request,'Display_Webpage.html',d)
+
+
+
+
+
+
+
+    
 
 def Display_Topic(request):
 
     QSTO=Topic.objects.all()
+    QSTO=Topic.objects.all().order_by('Topic_name')
+
     d={'QSTO':QSTO}
     return render(request,'Display_Topic.html',d)
 
+
+   
 
 
 
