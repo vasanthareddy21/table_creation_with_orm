2266d04e6c5d84366785fb3a264bac0f3e40c72c
vasanthareddy21 <vasanthareddya9@gamil.com>
2023-08-28 14:15:12 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -10,8 +10,28 @@ def Display_AccessRecords(request):
     QSARO=AccessRecords.objects.all()
     QSARO=AccessRecords.objects.all().order_by('name')
 
+    #year lookup
+    QSARO=AccessRecords.objects.filter(date__year='1999')
+
+    #month lookup
+    QSARO=AccessRecords.objects.filter(date__month='11')
+
+    #date lookup
+    QSARO=AccessRecords.objects.filter(date__day='11')
+
+    #regex lookup
+    QSARO=AccessRecords.objects.filter(author__regex='^s\w+')
+
+    #multiple lookups in single coloumn
+    QSARO=AccessRecords.objects.filter(date__year__lte='1999')
+
+    QSARO=AccessRecords.objects.filter(date__year__gte='1999')
+
+
     d={'QSARO':QSARO}
     return render(request,'Display_AccessRecords.html',d)
+
+
 
 def Display_Webpage(request):
     
@@ -33,10 +53,26 @@ def Display_Webpage(request):
 
     #we use filter metod[it will fetch only Boxing]
     QSWO=Webpage.objects.exclude(Topic_name='Boxing')
+    
+
+    #it deals with field lookups
+
+    #startswith fieldlookup
+    QSWO=Webpage.objects.filter(name__startswith='v')
+    QSWO=Webpage.objects.filter(name__startswith='s')
+
+    #endswith fieldlookup
+    QSWO=Webpage.objects.filter(url__endswith='a')
+    QSWO=Webpage.objects.filter(url__endswith='com')
+    QSWO=Webpage.objects.filter(url__endswith='in')
+
+    #contains lookup
+    QSWO=Webpage.objects.filter(url__contains='r')
+    QSWO=Webpage.objects.exclude(url__contains='r')
+    QSWO=Webpage.objects.filter(name__contains='r')
 
     d={'QSWO':QSWO}
     return render(request,'Display_Webpage.html',d)
-
 
 
 
